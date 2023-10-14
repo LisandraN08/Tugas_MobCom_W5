47a85fa898f8ab17ae11a8cb7351ea32696453e6
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-10-14 23:33:19 +0700

    sudah berhasil menampilkan 10 soal dengan nama negara yg berbeda-beda

--- TestW04/ContentView.swift
+++ TestW04/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  TestW04
 //
-//  Created by Christian on 06/10/23.
+//  Created by MacBook Pro on 14/10/23.
 //
 
 import SwiftUI
@@ -10,89 +10,108 @@ import SwiftUI
 struct ContentView: View {
     
     var asean = ["Indonesia", "Singapore", "Malaysia", "Laos", "Philipines", "Cambodia", "Myanmar", "Thailand", "Brunei", "Vietnam"]
-    var angkaRandom = Int.random(in: 0...9)
+    
+    @State private var currentQuestionIndex = 0
+    @State private var score = 0
+    @State private var isAnswerCorrect = false
+    @State private var showingAlert = false
+    @State private var shuffledQuestions = [Int]()
     
     var body: some View {
-        ZStack{
-            Color.mint
+        Text("ASEAN FLAGS GAME")
+            .fontWeight(.bold)
+            .font(.title)
+            .foregroundColor(.black)
+        
+        ZStack {
+            Color.pink
                 .ignoresSafeArea()
-            VStack{
-                Text("Pilih Bendera dari Negara : ")
-                    .foregroundStyle(.black)
-                Text(asean[angkaRandom])
-                    .foregroundStyle(.black)
+            VStack {
+                if currentQuestionIndex < shuffledQuestions.count {
+                    Text("Pilih Bendera dari Negara : ")
+                        .foregroundColor(.white)
+                    Text(asean[shuffledQuestions[currentQuestionIndex]])
+                        .foregroundColor(.white)
+                } else {
+                    Text("Permainan Selesai")
+                        .foregroundColor(.white)
+                    Text("Skor Anda: \(score)")
+                        .foregroundColor(.white)
+                }
             }
-          
         }
+        .frame(width: 800, height: 250)
         
-        HStack{
+        HStack {
             Spacer()
-            VStack{
+            VStack {
                 ForEach(0..<5) { number in
-                    Button {
-                        
-                    } label: {
-                        
+                    Button(action: {
+                        self.checkAnswer(number)
+                    }) {
                         Image(asean[number])
                             .resizable()
-                            .frame(width:105,height: 65)
+                            .frame(width: 105, height: 65)
                     }
                 }
             }
             Spacer()
-            VStack{
+            VStack {
                 ForEach(5..<10) { number in
-                    Button {
-                        
-                    } label: {
-                        
+                    Button(action: {
+                        self.checkAnswer(number)
+                    }) {
                         Image(asean[number])
                             .resizable()
-                            .frame(width:105,height: 65)
+                            .frame(width: 105, height: 65)
                     }
                 }
             }
             Spacer()
         }
         
+        .alert(isPresented: $showingAlert) {
+            if currentQuestionIndex >= asean.count {
+                return Alert(title: Text("Selamat!"), message: Text("Skor Anda: \(score)"), dismissButton: .default(Text("Selesai")) {
+                })
+            } else {
+                return Alert(title: Text(isAnswerCorrect ? "Jawaban Benar" : "Jawaban Salah"), message: Text("Skor Anda: \(score)"), dismissButton: .default(Text("Lanjut")) {
+                    self.showNextQuestion()
+                })
+            }
+        }
+        .onAppear {
+            self.shuffleQuestions()
+        }
+    }
         
+    func checkAnswer(_ selectedFlagIndex: Int) {
+        if currentQuestionIndex < shuffledQuestions.count {
+            if selectedFlagIndex == shuffledQuestions[currentQuestionIndex] {
+                score += 1
+                isAnswerCorrect = true
+            } else {
+                isAnswerCorrect = false
+            }
+            showingAlert = true
+        }
     }
     
-    func printConsole() {
-        print("Hai")
+    func shuffleQuestions() {
+        shuffledQuestions = Array(0..<asean.count).shuffled()
     }
     
-    func buttonBerbahaya(){
-        print("wow")
+    func showNextQuestion() {
+        currentQuestionIndex += 1
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
 #Preview {
     ContentView()
 }
-
-
-//VStack{
-//    Button("Submit", role: .cancel ,action: printConsole)
-//        .buttonStyle(.bordered)
-//        
-//    Button("Delete", role: .destructive){
-//        alertShown = true
-//    } .alert("BAHAYA", isPresented: $alertShown){
-//        Button("Wah!", role: .cancel) {}
-//        Button("Kabor!", role: .destructive) {}
-//    } message: {
-//        Text("Mangga dibaca")
-//    }
-//        .buttonStyle(.borderedProminent)
-//        
-//    
-//    
-//    Button{
-//        print("Coba")
-//    } label: {
-//        Text("Compose")
-//        Image(systemName: "mail")
-//    }
-//    .buttonStyle(.bordered)
-//}
